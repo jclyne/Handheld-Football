f9da9901e25670a1806793cdc39d37d7521bee7d
Jeff Clyne <jclyne@gvengdt03.GLOBALVELOCITY.LOCAL>
2011-06-03 16:19:29 -0500

    Refactoring and javadoc

--- src/com/redpantssoft/TextViewAnimator.java
+++ src/com/redpantssoft/TextViewAnimator.java
@@ -1,126 +1,270 @@
 package com.redpantssoft;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 import android.view.animation.Animation;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 
-public class TextViewAnimator implements Animation.AnimationListener,Timer.TimerHandler
-{
-	TextView mTextView;
-	Animation mInAnim;
-	Animation mOutAnim;
-	
-	boolean mActive=false;
-	
-	Timer mTimer;
-	
-	class TextDisplay
-	{
-		private String mText;
-		private int mDisplayMillis;
-		
-		TextDisplay(String text )
-		{
-			mText = text;
-			mDisplayMillis = 0;
-		}
-		
-		TextDisplay(String text, int displayMillis )
-		{
-			mText = text;
-			mDisplayMillis = displayMillis;
-		}
-		
-		public final String getText() { return mText; }
-		public final int getDisplayMillis() { return mDisplayMillis; }
-	}
-	Queue<TextDisplay> mTextQueue;
-	
-	public TextViewAnimator(TextView textView,Animation inAnim,Animation outAnim)
-	{
-		mTextView=textView;
-		mInAnim=inAnim;
-		mOutAnim=outAnim;
-		
-		mTextQueue = new LinkedList<TextDisplay>();
-		mInAnim.setAnimationListener(this);
-		mOutAnim.setAnimationListener(this);
-		mTimer=new Timer(this);
-	}
-	
-	public void onAnimationEnd(Animation animation) 
-	{
-		if (animation == mOutAnim)
-		{
-			mTextView.setBackgroundColor(0x00000000);
-			mTextView.setText("");
-			if (! mTextQueue.isEmpty() )
-			{
-				mTextView.startAnimation(mInAnim);
-				mTextView.invalidate();
-			}
-			else
-				mActive=false;
-		}
-	}
+/**
+ * Provides an animated TextView
+ * <p/>
+ * This class wraps a text view and provides animations for drawing
+ * and clearing the text.  The Text can automatically clear after
+ * a set period of milliseconds, are be manually cleared. The delay for
+ * automatically clearing is implemented on a UI Timer.
+ * <p/>
+ * It also implements a queue of text strings if numerous setText requests
+ * are made. A text string will remain visible until manually cleared, or
+ * the display millisecond value expires.
+ * <p/>
+ *
+ * @author Jeff clyne
+ */
+public class TextViewAnimator implements Animation.AnimationListener, Timer.TimerHandler {
+    /**
+     * TextView that is wrapped and animated
+     */
+    private final TextView textView;
 
-	public void onAnimationRepeat(Animation animation) 
-	{
-		
-	}
+    /**
+     * Animation for initial display of text
+     */
+    private final Animation inAnim;
 
-	public void onAnimationStart(Animation animation) 
-	{
-		if (animation == mInAnim)
-		{
-			TextDisplay disp=mTextQueue.remove();
-			mTextView.setBackgroundColor(0x50000000);
-			mTextView.setText(disp.getText());
-			if (disp.getDisplayMillis() > 0)
-				mTimer.start(disp.getDisplayMillis());
-		}
-	}
-	
-	public boolean HandleTimer() 
-	{
-		clearText();
-		return false;
-	}
+    /**
+     * Animation to clean up the text
+     */
+    private final Animation outAnim;
 
-	public void setText(String text) 
-	{
-		mTextQueue.add(new TextDisplay(text,0));
-		if (!mActive)
-		{
-			mActive=true;
-			mTextView.startAnimation(mInAnim);
-			mTextView.invalidate();
-		}
-	}
-	
-	public void setText(String text,int millis) 
-	{
-		mTextQueue.add(new TextDisplay(text,millis));
-		if (!mActive)
-		{
-			mActive=true;
-			mTextView.startAnimation(mInAnim);
-			mTextView.invalidate();
-		}
-	}
-	
-	public void clearText() 
-	{
-		mTextView.startAnimation(mOutAnim);
-		mTextView.invalidate();
-	}
-	
-	public void clear()
-	{
-		mTextQueue.clear();
-		clearText();
-	}
+    /**
+     * Specifies the background color of the textView when text is displayed
+     * This is mainly used when the textView is layered in a  FrameLayout.
+     * This default is a translucent black to darken the background to  make
+     * the text more readable.
+     */
+    private int backgroundColor = 0x50000000;
+    /**
+     * Timer to handle automatic clearing of the animated text
+     */
+    private final Timer timer;
+
+    /**
+     * Flag that indicates whether or not an animation is active
+     */
+    private boolean active = false;
+
+    /**
+     * Class that represents a queued string to be displayed. It contains
+     * the text string as well as the desired display time in milliseconds.
+     */
+    class TextDisplay {
+        /**
+         * Text string to be displayed
+         */
+        private final String mText;
+
+        /**
+         * Time to display string before auto cleanup
+         */
+        private final int mDisplayMillis;
+
+        /**
+         * Constructor for an auto cleanup item
+         *
+         * @param text          text string to display
+         * @param displayMillis time to display the string in milliseconds
+         */
+        TextDisplay(String text, int displayMillis) {
+            mText = text;
+            mDisplayMillis = displayMillis;
+        }
+
+        /**
+         * Accessor for Display Text
+         *
+         * @return text string to displayed for this item
+         */
+        public final String getText() {
+            return mText;
+        }
+
+        /**
+         * Accessor for Display time
+         *
+         * @return time to display the text before auto cleanup
+         */
+        public final int getDisplayMillis() {
+            return mDisplayMillis;
+        }
+    }
+
+    /**
+     * Queue containing the TextDisplay objects to be displayed
+     */
+    private final Queue<TextDisplay> textQueue;
+
+    /**
+     * Constructor
+     *
+     * @param textView specifies an existing text view to wrap
+     * @param inAnim   animation to be used to display the text in the view
+     * @param outAnim  animation to be used to to clear the text in the view
+     */
+    public TextViewAnimator(TextView textView, Animation inAnim, Animation outAnim) {
+        this.textView = textView;
+        this.inAnim = inAnim;
+        this.outAnim = outAnim;
+
+        // Use the linked list implementation of Queue
+        textQueue = new LinkedList<TextDisplay>();
+
+        // This object acts as the listener for the animation events
+        // as well as the timer events
+        this.inAnim.setAnimationListener(this);
+        this.outAnim.setAnimationListener(this);
+        timer = new Timer(this);
+    }
+
+    /**
+     * Displays text in the view
+     * Text displayed with this method needs
+     * to be manually cleared with {@link #clearText() clearText}
+     *
+     * @param text string to display
+     * @see #setText(String text, int millis)
+     */
+    public void setText(String text) {
+        textQueue.add(new TextDisplay(text,0));
+        if (!active) {
+            active = true;
+            textView.startAnimation(inAnim);
+            textView.invalidate();
+        }
+    }
+
+    /**
+     * Displays text in the view
+     * Text displayed with this method will
+     * automatically cleared after millis milliseconds
+     *
+     * @param text   string to display
+     * @param millis milliseconds to display the text before clearing
+     * @see #setText(String)
+     */
+    public void setText(String text, int millis) {
+        textQueue.add(new TextDisplay(text, millis));
+        if (!active) {
+            active = true;
+            textView.startAnimation(inAnim);
+            textView.invalidate();
+        }
+    }
+
+    /**
+     * Clears the currently displayed text.
+     * This can be used with a text item that is automatically cleared as well
+     */
+    public void clearText() {
+        if (!active) return;
+
+        timer.stop();
+        textView.startAnimation(outAnim);
+        textView.invalidate();
+    }
+
+    /**
+     * Clears all text in the Queue as well as the currently displayed
+     * string.
+     */
+    public void clear() {
+        textQueue.clear();
+        clearText();
+    }
+
+    /**
+     * Getter for the background color
+     *
+     * @return  current background color
+     */
+    public final int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * Setter for the background color
+     *
+     * @param backgroundColor new background color
+     */
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+
+    /**
+     * Animation end handler.
+     * When the out animation is completed, this will check the text
+     * queue and start the display the next item, if one exists.
+     * Regardless it will clear the text and background
+     *
+     * @param animation specifies which animation is ending
+     */
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if (animation == outAnim) {
+            textView.setBackgroundColor(0);
+            textView.setText("");
+            if (!textQueue.isEmpty()) {
+                textView.startAnimation(inAnim);
+                textView.invalidate();
+            } else
+                active = false;
+        }
+    }
+
+    /**
+     * Animation Repeat Handler
+     * N/A
+     *
+     * @param animation specifies which animation is repeating
+     */
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    /**
+     * Animation Start Handler
+     * For the  in animation, this will retrieve the text display item
+     * from the queue
+     *
+     * @param animation specifies which animation is starting
+     */
+    @Override
+    public void onAnimationStart(Animation animation) {
+        if (animation == inAnim) {
+            try {
+                TextDisplay d = textQueue.remove();
+                textView.setBackgroundColor(backgroundColor);
+                textView.setText(d.getText());
+                if (d.getDisplayMillis() > 0)
+                    timer.start(d.getDisplayMillis());
+            } catch (NoSuchElementException e) {
+                animation.cancel();
+            }
+        }
+    }
+
+    /**
+     * Auto clear timer handler
+     * This just automates calling clearText
+     *
+     * @return boolean to indicate if the timer should re-armed
+     */
+    @Override
+    public boolean HandleTimer() {
+        clearText();
+        return false;
+    }
 }
--- src/com/redpantssoft/Timer.java
+++ src/com/redpantssoft/Timer.java
@@ -3,61 +3,110 @@ package com.redpantssoft;
 import android.os.Handler;
 import android.os.Message;
 
-public class Timer extends Handler
-{
-	private boolean mRunning=false;
-	private int mIntervalMillis=0;
-	private TimerHandler mHandler;
-	private static int GLOBAL_MESSAGE_ID=0;
-	private int mMessageId;
-	
-	public interface TimerHandler
-	{
-		public boolean HandleTimer();
-	}
-	
-	public Timer(TimerHandler handler)
-	{
-		mMessageId=GLOBAL_MESSAGE_ID++;
-		mHandler=handler;
-	}
-	
-	public Timer(int intervalMillis, TimerHandler handler)
-	{
-		mMessageId=GLOBAL_MESSAGE_ID++;
-		mHandler=handler;
-		mIntervalMillis=intervalMillis;
-	}
-	
-	@Override
-	public void handleMessage(Message msg)
-	{
-		if ( (msg.what == mMessageId) && mRunning)
-		{
-			mRunning=false;
-			if ( mHandler.HandleTimer())
-			{
-				start(mIntervalMillis);
-			}
+/**
+ * Implementation of  a timer based on the Android send message/handler api.
+ *
+ * A user implements the Timer.TimerHandler interface which
+ * takes care of handling the timer expiration.  It can
+ * be optionally re-armed for a continuously repeating timer
+ *
+ * @author Jeff clyne
+ */
+public class Timer extends Handler {
 
-		}
-	}
+    /**
+     * Represents a global message ID for timer messages
+     */
+    private static final int TIMER_MESSAGE_ID = 1024;
 
-	public void start(int intervalMillis)
-	{
-		mIntervalMillis=intervalMillis;
-		start();
-	}
-	
-	public void start()
-	{
-		mRunning=true;
-		removeMessages(mMessageId);
-		sendMessageDelayed(obtainMessage(mMessageId), mIntervalMillis);
-	}
-	
-	public void stop()
-	{
-		mRunning=false;
-	}	
+    /**
+     * Flag that indicates whether or not the timer is armed
+     */
+    private boolean running = false;
+
+    /**
+     * Specifies the current timer interval. This can be
+     * initialized at timer creation and changed any time
+     * the timer is started.
+     */
+    private int intervalMillis = 0;
+
+    /**
+     * Reference to the timer handler, set at timer creation
+     */
+    private final TimerHandler handler;
+
+    /**
+     * Interface to handle the expiration of a timer
+     */
+    public interface TimerHandler {
+        /**
+         * Method should be implemented to handle expiration of a timer
+         * @return true to re-arm the timer
+         */
+        public boolean HandleTimer();
+    }
+
+    /**
+     * Constructor
+     *
+     * @param handler reference to the timer handler
+     */
+    public Timer(TimerHandler handler) {
+        this.handler = handler;
+    }
+
+    /**
+     * Constructor
+     * @param intervalMillis  initial value of timer interval
+     * @param handler  reference to the timer handler
+     */
+    public Timer(int intervalMillis, TimerHandler handler) {
+        this.handler = handler;
+        this.intervalMillis = intervalMillis;
+    }
+
+    /**
+     * Starts the timer with a specified millisecond interval
+     *
+     * @param intervalMillis timer interval to arm timer with
+     */
+    public void start(int intervalMillis) {
+        this.intervalMillis = intervalMillis;
+        start();
+    }
+
+    /**
+     * Starts the timer with the previously specified interval
+     */
+    public void start() {
+        running = true;
+        removeMessages(TIMER_MESSAGE_ID,this);
+        sendMessageDelayed(obtainMessage(TIMER_MESSAGE_ID, this), intervalMillis);
+    }
+
+    /**
+     * Stops the current timer
+     */
+    public void stop() {
+        running = false;
+    }
+
+    /**
+     * Implementation of the android.handler.handleMessage method. This
+     * is called to handle messages delivered to the looper message queue.
+     *
+     * @param msg   message being delivered from looper thread
+     */
+    @Override
+    public void handleMessage(Message msg) {
+        if (running && (msg.what == TIMER_MESSAGE_ID) && (msg.obj == this)) {
+            running = false;
+            if (handler.HandleTimer()) {
+                start(intervalMillis);
+            }
+        }
+    }
+
+
 }
